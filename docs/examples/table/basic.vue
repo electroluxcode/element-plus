13be92dc4e48c18ad2a9fc40e10511386b616cc8
Electrolux <3451613934@qq.com>
2024-04-16 09:05:13 +0800

    feat(demo): add demo for codesandbox

--- docs/examples/table/basic.vue
+++ docs/examples/table/basic.vue
@@ -1,32 +1,50 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
-  </el-table>
+  <div class="tran">
+    <div class="left">dsad</div>
+    <div class="right">
+      <div>
+        <el-table
+          :data="tableData"
+          style="width: 100%"
+          :show-overflow-tooltip="{
+            placement: 'bottom',
+          }"
+          tooltip-effect="light"
+          row-key="id"
+        >
+          <el-table-column prop="date" label="Date" width="100" />
+          <el-table-column prop="name" label="Name" width="200" />
+        </el-table>
+      </div>
+    </div>
+  </div>
 </template>
-
+干坞
+<style>
+.tran {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  height: 400px;
+}
+.right {
+  transform-origin: left top;
+  transform: scale(1.6);
+}
+</style>
 <script lang="ts" setup>
 const tableData = [
   {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "N",
+    name: "No2323",
+    address: "No. ",
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date:
+      "No. 189xLos AngelesNo. 189, Grove St, Los AngelesNo. 189, Grove St, Los AngelesNo. 189, Grove St, Los AngelesNo. 189, Grove St, Los Angeles",
+    name:
+      "No. 189, Grove St, Los Angeles No. 189, Grove St, Los AngelesNo. 189, Grove St, Los AngelesNo. 189, Grove St, Los AngelesNo. 189, Grove St, Los AngelesNo. 189, Grove St, Los AngelesNo. 189, Grove St, Los Angeles",
+    address:
+      "No. 189, Grove St, Los Angeles No. 189, Grove St, Los AngelesNo. lesNo. 189, Grove St, Los AngelesNo. 189, Grove St, Los Angeles",
   },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-]
+];
 </script>
